124bb6c3b2c1f7532c2d05b22111af5d70ba7548
jjangsangy <jjangsangy@gmail.com>
2013-11-15 01:26:32 -0800

    pin update

--- bin/pin_by_date.sh
+++ bin/pin_by_date.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Awk script that parses for dates. Not advised to be used over network
-# since it will be highly IO bound
+# Awk script that parses for dates.
+# Not advised to be used over network
 
+# TODO: Merge into main pin script. Parameterize with
+# getopts.
 pinDate="${1}"
 pinMonth=$(cut -d "/" -f1 <(echo "${pinDate}"))
 find . -name '*.csv' -type f -print0 | xargs -0 awk -F "," '\
