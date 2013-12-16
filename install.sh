933a55def8d0861c284d6004a2f7109a990825ec
Sang Han <jjangsangy@gmail.com>
2013-12-15 20:26:56 -0800

    init

--- install.sh
+++ install.sh
@@ -1,64 +1,102 @@
 #!/bin/bash
+#
+# FILE:         install
+# AUTHOR:       Sang Han
+# VERSION:      1.0.0rc
+# DESCRIPTION   Install utility for dotfile configs
+# DEPENDS:      Dotfiles 
 
-if [[ $(uname -s) == "Linux" ]]; then
-    APTPREINSTALL=(git git-core vim zsh curl wget)
-    clear
-    echo "Please Select:
+usage() {
+    cat <<- EOF
+    $(basename $0) [-h help] [-s silent]
 
-    1. Update & Install?
-    2. Only Install?
-    0. Quit
-    "
-    read -p "Enter selection [1-3]: " RESP
-    case ${RESP} in
-        1)  echo "Updating System"
-            sudo apt-get update && sudo apt-get upgrade && echo "System is now up to date"
-        echo "Now installing ${APTPREINSTALL[*]}" && sudo apt-get install ${APTPREINSTALL[*]}
-        ;;
-        2)  echo "Now installing ${APTPREINSTALL[*]}" && sudo apt-get install ${APTPREINSTALL[*]}
-        ;;
-        0)  echo "Exiting"
-        exit
-        ;;
-        *)  echo "Sorry does not match any selections"
+    Install utility for dotfile configs
 
-        esac
-fi
+    AUTHOR:     Sang Han
+    YEAR:       2013
+    VERSION:    1.0.0rc
 
-if [[ $(uname -s) == "Darwin" ]]; then
-        which -s brew
-        if [[ $? != 0 ]]; then
-                BREWINSTALL=(curl git ctags wget zsh vim tmux python)
-                echo "Installing Homebrew..."
-                ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-                brew update
-                    for apps in "${BREWINSTALL[@]}"; do
-                    brew install ${apps}
+    -h [help]
+        Outputs usage directions
+
+
+EOF
+    exit 0
+}
+
+# Parse Options
+while getopts ":h" OPTION; do
+    case ${OPTION} in
+        h) usage
+            ;;
+        ?) echo "Invalid option: -${OPTARG}" >&2
+            exit 1
+            ;;
+    esac
+done
+    shift $(($OPTIND-1))
+
+old_script() {
+    if [[ $(uname -s) == "Linux" ]]; then
+        APTPREINSTALL=(git git-core vim zsh curl wget)
+        clear
+        echo "Please Select:
+
+        1. Update & Install?
+        2. Only Install?
+        0. Quit
+        "
+        read -p "Enter selection [1-3]: " RESP
+        case ${RESP} in
+            1)  echo "Updating System"
+                sudo apt-get update && sudo apt-get upgrade && echo "System is now up to date"
+            echo "Now installing ${APTPREINSTALL[*]}" && sudo apt-get install ${APTPREINSTALL[*]}
+            ;;
+            2)  echo "Now installing ${APTPREINSTALL[*]}" && sudo apt-get install ${APTPREINSTALL[*]}
+            ;;
+            0)  echo "Exiting"
+            exit
+            ;;
+            *)  echo "Sorry does not match any selections"
+
+            esac
+    fi
+
+    if [[ $(uname -s) == "Darwin" ]]; then
+            which -s brew
+            if [[ $? != 0 ]]; then
+                    BREWINSTALL=(curl git ctags wget zsh vim tmux python)
+                    echo "Installing Homebrew..."
+                    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+                    brew update
+                        for apps in "${BREWINSTALL[@]}"; do
+                        brew install ${apps}
+                        done
+            fi
+
+            if ! [[ -d "$HOME/Dotfiles" ]]; then
+                git clone https://github.com/jjangsangy/Dotfiles.git "$HOME/Dotfiles"
+            fi
+
+            source $HOME/Dotfiles/osx/osx.sh
+
+            if ! [[ -d ${HOME}/bin/maximum-awesome ]]; then
+                    echo "Installing Maximum Awesome VIM"
+                    maxDIR="${HOME}/bin/maximum-awesome"
+                    dotDIR="${HOME}/Dotfiles"
+                    myVIMRC=("${dotDIR}/vim/vimrc" "${dotDIR}/vim/vimrc.local")
+                    git clone https://github.com/square/maximum-awesome.git "${maxDIR}" && cd "${maxDIR}" && rake
+
+                    for file in "${myVIMRC[@]}"; do
+                    origFile="${HOME}/.${file##*/}"
+                        if [[ -f "${file}" ]]; then
+                              if [[ -f "${origFile}" ]]; then
+                                    rm "${origFile}"
+                              fi
+                              ln "${file}" "${origFile}"
+                        fi
                     done
-        fi
+            fi
 
-        if ! [[ -d "$HOME/Dotfiles" ]]; then
-            git clone https://github.com/jjangsangy/Dotfiles.git "$HOME/Dotfiles"
-        fi
-
-        source $HOME/Dotfiles/osx/osx.sh
-
-        if ! [[ -d ${HOME}/bin/maximum-awesome ]]; then
-                echo "Installing Maximum Awesome VIM"
-                maxDIR="${HOME}/bin/maximum-awesome"
-                dotDIR="${HOME}/Dotfiles"
-                myVIMRC=("${dotDIR}/vim/vimrc" "${dotDIR}/vim/vimrc.local")
-                git clone https://github.com/square/maximum-awesome.git "${maxDIR}" && cd "${maxDIR}" && rake
-
-                for file in "${myVIMRC[@]}"; do
-                origFile="${HOME}/.${file##*/}"
-                    if [[ -f "${file}" ]]; then
-                          if [[ -f "${origFile}" ]]; then
-                                rm "${origFile}"
-                          fi
-                          ln "${file}" "${origFile}"
-                    fi
-                done
-        fi
-
-fi
+    fi
+}
