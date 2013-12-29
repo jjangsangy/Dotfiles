9f6fc9f42788732ee2d6dfab0fc32289c5374bbc
jjangsangy <jjangsangy@gmail.com>
2013-12-28 17:14:20 -0800

    functions

--- bash/jump.sh
+++ bash/jump.sh
@@ -1,26 +1,30 @@
 # Checks for marks directory, and creates it
-if ! [[ -d $HOME/.marks ]]; then
-    echo "No path for jump"
+export MARKPATH=$HOME/.marks
+if ! [[ -d "$MARKPATH" ]]; then
+    echo "No directory at $MARKPATH"
     echo "Directory being created"
-    mkdir $HOME/.marks
+    mkdir -p "$MARKPATH"
 fi
 
-export MARKPATH=$HOME/.marks
 
-function jump {
+jump() {
     cd -P "$MARKPATH/$1" 2> /dev/null || echo "No such mark: $1"
 }
 
-function mark {
-    mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
+mark() {
+    local MARKING="$MARKPATH/$1"
+    ln -s "$(pwd)" "$MARKING" || \
+        printf "Could not symlink %s to %s" "$(pwd)" "$MARKING"
 }
 
-function unmark {
+unmark() {
     rm -i "$MARKPATH/$1"
 }
 
-function marks {
-    ls -l $MARKPATH | sed -e 's:\  : :g' -e 's:@::g' | cut -f 9- -d ' ' | awk 'BEGIN {FS="->"}; NR>1 {printf("%-3.2d %-24s %s %s\n",NR-1,$1,"->",$2)}'
+marks() {
+    ls -l $MARKPATH | sed -e 's:\  : :g' -e 's:@::g' | cut -f 9- -d ' ' | \
+    awk \
+        'BEGIN {FS="->"}; NR>1 {printf("%-3.2d %-24s %s %s\n",NR-1,$1,"->",$2)}'
 }
 
 _completemarks() {
