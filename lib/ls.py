a1878f7e6c64cac5246b2beee6b8df10cfac02fc
jjangsangy <jjangsangy@gmail.com>
2014-01-12 08:19:18 -0800

    also parse filetype

--- lib/ls.py
+++ lib/ls.py
@@ -5,15 +5,21 @@ import os
 def main():
     u = optparse.OptionParser(
             description = 'Python clone of ls command. Prints out a list of files in a directory separated by newlines.',
-            prog    = "ls.py",
-            version     = "1.0rc",
+            prog        = "ls.py",
+            version     = "1.1rc",
             usage       = "%prog [directory]"
             )
     (options, arguments) = u.parse_args()
+    path = arguments[0]
     if len(arguments) == 1:
-        path = arguments[0]
-        for filename in os.listdir(path):
-            print filename
+        for pathname in os.listdir(path):
+            print pathname
+    elif len(arguments) == 2:
+        from glob import glob
+        filetype = arguments[1]
+        for pathname in glob(os.path.join(path,'*')+filetype):
+            filename = pathname.split('/')[-1]
+            print filename.split('.'+filetype)[0]
     else:
         u.print_help()
 
