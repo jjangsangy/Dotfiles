8cd93e30f7f7cd373aafa784c67b04824bfec853
jjangsangy <jjangsangy@gmail.com>
2014-01-17 03:59:40 -0800

    refactored without using glob module

--- lib/ls.py
+++ lib/ls.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python
 import optparse
 import os
-import log_conf, logging
+import sys
+
+optarg = optparse.OptionParser(
+        description = 'Python clone of ls command. Prints out a list of files in a directory separated by newlines.',
+        prog        = "ls.py",
+        version     = "1.2rc",
+        usage       = "%prog [directory]"
+        )
+optarg.add_option('-f', '--filetype', help='selects specific filetype extensions')
+(options, arguments) = optarg.parse_args()
 
 def main():
-    u = optparse.OptionParser(
-            description = 'Python clone of ls command. Prints out a list of files in a directory separated by newlines.',
-            prog        = "ls.py",
-            version     = "1.1rc",
-            usage       = "%prog [directory]"
-            )
-    u.add_option('-f', '--filetype', help='selects specific filetype extensions')
-    (options, arguments) = u.parse_args()
-    logging.debug('options are %s', options)
-    logging.debug('arguments are %s', arguments)
-
     if len(arguments) == 1:
         path = arguments[0]
-        if options.filetype is not None:
-            filetype = options.filetype
-        else:
-            filetype = str()
-
-        from glob import glob
-        pathname = os.path.join(path,'*')
-        for path in glob(pathname+filetype):
-            filename = path.split('/')[-1]
-            print filename
+        filetype, filenames = options.filetype, os.listdir(path)
+        if filetype is not None:
+            filenames = [
+                name for name in filenames if name.endswith(tuple(filetype))
+                ]
+        for file in filenames:
+            sys.stdout.write('%s\n' % file)
     else:
-        u.print_help()
-
+        optarg.print_help()
+        sys.stderr.write('ls.py only takes 1 argument' + '\n')
 
 if __name__ == '__main__':
-    log_conf.setup_logging()
     main()
