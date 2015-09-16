8b4bffb80b4a42fb5f74347478e5e1b221234acb
Sang Han <jjangsangy@gmail.com>
2015-09-16 02:46:47 -0700

    Updated Linker

--- link.sh
+++ link.sh
@@ -2,32 +2,35 @@
 # ==============================================================================
 usage() { cat <<- DOCUMENT
 
-    usage: $PROGNAME [-h help] [-t test] [-f file]
+    Usage: $PROGNAME [-h] [-t] [-c link.conf]
 
     AUTHOR:      Sang Han
     CREATED:     01/09/2014
     REVISION:    1.4
 
-    $COLOR DESCRIPTION:
+    DESCRIPTION:
         Program for automating users preferred login shell enviornment.
         Symbolically links the startup files located within dotfiles
         repository and links them to to users \$HOME variable prepended
         with a dot. If the startup file already and a collision occurs,
         user will be prompted for deletion.
 
+    HOME DIRECTORY: $HOME
+
     REQUIREMENTS:
-        Program must be called while present working directory is within
-        the root of the Dotfiles repository.
+        - link.conf (Config File)
 
     OPTIONS:
         -h [help]
             Outputs usage directions
         -t [test]
-            Runs internal unit tests
+            A dry run, doesn't actually make changes to the filesystem
         -v [verbose]
             Prints out all test output to screen
-        -f [file]
-            Reference external file
+        -c [config]
+            Reference external configuration file as metadata
+        -f [force]
+            Always replace files: Warning this is dangerous
 
     EXAMPLES:
         Run unit tests and prints out all assigned values and variables
@@ -37,12 +40,7 @@ usage() { cat <<- DOCUMENT
         ./$PROGNAME
 
         Use your own config file:
-        ./$PROGNAME -f config_file
-
-    TODO: Allow the usage of a configuration file to specified rather than
-          just reading from global variables.
-    TODO: Allow the option of copying the files rather than symbolic linking
-
+        ./$PROGNAME -c config_file
 
 	DOCUMENT
 }
@@ -97,44 +95,47 @@ test_dest() {
 }
 
 function prompt_delete() {
-# ===============================================================================
-# Prompts the user authorization for deleting original file at $LINK_DEST.
-# After authorization is granted, file is deleted and replaced with
-# a symlink from $LINK_SOURCE
-# ===============================================================================
+
+    if ((FORCE==1)); then
+        rm -rf "${LINK_DEST}" 2>/dev/null
+        link_files
+        return
+    fi
+
     read -p "File $LINK_DEST already exists, would you like to delete it? \
         [Yy]/[Nn]:  " RESPONSE
 
     if [[ $RESPONSE =~ [Yy] ]]; then
-        rm "${LINK_DEST}"
-        link_files
+        rm "${LINK_DEST}" && link_files
     else
         return
     fi
 }
 
+# ===========================================================================
+# Link Files
+# ===========================================================================
 function link_files() {
-    # ===========================================================================
-    # Link Files
-    # ===========================================================================
     ln -s "${LINK_SOURCE}" "${LINK_DEST}"
 }
 
 # ===============================================================================
 # Parameters
 # ===============================================================================
-declare -i TEST=0 VERBOSE=0
+declare -i TEST=0 VERBOSE=0 FORCE=0
 declare -a FILELIST
-while getopts "f:htv" OPTION; do
+while getopts "c:htvf" OPTION; do
     case ${OPTION} in
         h) usage
            exit 0
             ;;
         t) TEST=1
             ;;
-        f) CONFIG_FILE="${OPTARG}"
+        c) CONFIG_FILE="${OPTARG}"
             ;;
         v) VERBOSE=1
+            ;;
+        f) FORCE=1
             ;;
        \?) echo "Invalid option: -${OPTARG}" >&2
            exit 1
@@ -147,23 +148,36 @@ done
 # Main
 # ===============================================================================
 main() {
+    # Grab Config File
     FILELIST=( $(cat "${CONFIG_FILE:-"${PROGDIR}/link.conf"}") )
+
     if ((TEST==1 || VERBOSE==1)); then
-        test_variables TEST PROGNAME PROGDIR FILELIST CONFIG_FILE; printf "\n"
+        test_variables TEST PROGNAME PROGDIR FILELIST CONFIG_FILE FORCE
+        printf "\n"
     fi
 
-    # If file is located within a subdirectory
-    # the name of parent is stripped
+    # Parent directory name is stripped
     for FILE in "${FILELIST[@]}"; do
+    {
         if [ -f "${PROGDIR}/${FILE}" ]; then
-            local LINK_SOURCE="${PROGDIR}/${FILE}"
-            local LINK_DEST="${HOME}/.${FILE##*/}"
+        {
+            local LINK_SOURCE="${PROGDIR}/${FILE}" LINK_DEST="${HOME}/.${FILE##*/}"
 
-            if ((TEST==1)); then test_source && test_dest; continue; fi
-            if ((VERBOSE==1)); then test_source && test_dest;fi
+            # print out source and destination only
+            if ((TEST==1)); then
+                test_source && test_dest
+                continue
+            fi
 
+            if ((VERBOSE==1)); then
+                test_source && test_dest
+            fi
+
+            # Magic
             link_files >/dev/null 2>&1 || prompt_delete
+        }
         fi
+    }
     done
 }
 
