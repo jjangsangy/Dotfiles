107081427bf43711ed604541670bb9ed560d4929
Sang Han <sanghan@protonmail.com>
2025-01-26 18:03:57 -0800

    modify fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,29 +1,31 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 function fish_greeting
-  fastfetch
+  if type --quiet fastfetch
+    fastfetch
+  end
 end
 
-test -x $HOME/miniconda3/bin/conda
-  and eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/usr/opt/google-cloud-sdk/path.fish.inc' ]; . '/usr/opt/google-cloud-sdk/path.fish.inc'; end
-
-if type --quiet z
+if type --quiet zoxide
   zoxide init fish --cmd cd | source
 end
 
 if type --quiet starship
+  set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
+
   function starship_transient_prompt_func
     starship module character
   end
   function starship_transient_rprompt_func
     starship module time
   end
+
   starship init fish | source
+
   enable_transience
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/usr/opt/google-cloud-sdk/path.fish.inc' ]; . '/usr/opt/google-cloud-sdk/path.fish.inc'; end
+
+test -x $HOME/miniconda3/bin/conda
+  and eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+
