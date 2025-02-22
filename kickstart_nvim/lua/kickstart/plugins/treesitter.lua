48a86d39af46a222965557ba1a7812f39402237f
Sang Han <sanghan@protonmail.com>
2025-02-21 17:21:19 -0800

    fix lsp

--- kickstart_nvim/lua/kickstart/plugins/treesitter.lua
+++ kickstart_nvim/lua/kickstart/plugins/treesitter.lua
@@ -1,38 +1,106 @@
-return { -- Highlight, edit, and navigate code
+-- Highlight, edit, and navigate code
+return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  main = "nvim-treesitter.configs", -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = {
-      "python",
-      "bash",
-      "c",
-      "diff",
-      "html",
-      "lua",
-      "luadoc",
-      "markdown",
-      "markdown_inline",
-      "query",
-      "vim",
-      "vimdoc",
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { "ruby" },
-    },
-    indent = { enable = true, disable = { "ruby" } },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
   },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      -- Add languages to be installed here that you want installed for treesitter
+      ensure_installed = {
+        "lua",
+        "python",
+        "javascript",
+        "typescript",
+        "vimdoc",
+        "vim",
+        "regex",
+        "terraform",
+        "sql",
+        "dockerfile",
+        "toml",
+        "json",
+        "java",
+        "groovy",
+        "go",
+        "gitignore",
+        "graphql",
+        "yaml",
+        "make",
+        "cmake",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "tsx",
+        "css",
+        "html",
+      },
+
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<c-space>",
+          node_incremental = "<c-space>",
+          scope_incremental = "<c-s>",
+          node_decremental = "<M-space>",
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
+      },
+    })
+
+    -- Register additional file extensions
+    vim.filetype.add({ extension = { tf = "terraform" } })
+    vim.filetype.add({ extension = { tfvars = "terraform" } })
+    vim.filetype.add({ extension = { pipeline = "groovy" } })
+    vim.filetype.add({ extension = { multibranch = "groovy" } })
+  end,
 }
