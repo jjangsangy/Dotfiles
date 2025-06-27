abcbdf58a4b523611dc7e1bc97e74a50e5cfbd8f
Sang Han <sanghan@protonmail.com>
2025-06-27 15:27:06 -0700

    consolidate bash

--- fish/functions/l.fish
+++ fish/functions/l.fish
@@ -1,3 +1,3 @@
 function l --wraps='eza --icons --group-directories-first -F -1' --description 'alias ls eza --icons --group-directories-first -F -1'
-  eza --icons --group-directories-first -F -1 $argv; 
+    eza --icons --group-directories-first -F -1 $argv
 end
